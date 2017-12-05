d2abb2d8f098fdfaa895a0c963d6d04f2b5c7ff6
Dan Harms <danielrharms@gmail.com>
2017-12-05 17:49:35 -0600

    add more utils and tests

--- parsenv-utils.el
+++ parsenv-utils.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <enniomore@icloud.com>
 ;; Created: Monday, December  4, 2017
 ;; Version: 1.0
-;; Modified Time-stamp: <2017-12-05 08:33:00 dharms>
+;; Modified Time-stamp: <2017-12-05 17:48:54 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords: tools
 ;; URL: https://github.com/articuluxe/parsenv.git
@@ -48,37 +48,23 @@
 
 (defun parsenv-utils-continuation-p (line)
   "Return non-nil if LINE ends in a continuation character `\'."
-  (string-match-p "\\$" line))
+  (string-match-p "\\\\$" line))
 
 (defun parsenv-utils-strip-continuation (line)
   "Strip any trailing continuation character `\' from LINE."
-  (when (string-match "\\$" line)
-    (replace-match "" nil nil line 0)))
+  (if (string-match "\\\\$" line)
+      (replace-match "" nil nil line)
+    line))
+
+(defun parsenv-utils-strip-export (line)
+  "Strip any `export=' prefix from LINE."
+  (if (string-match "^\\s-*export\\s-+" line)
+      (replace-match "" nil nil line)
+    line))
 
 
 
 
-
-
-(defun parsenv-utils-load-env-vars-from-file (file)
-  "Load each line from FILE, of the form `var=val'.
-For each line, sets environment variable `var' equal to `val'."
-  (interactive "fLoad environment variables from file: ")
-  (mapc (lambda(line)
-          (when (string-match "\\(.+\\)=\\(.*\\)" line)
-            (setenv (match-string-no-properties 1 line)
-                    (substitute-env-vars
-                     (match-string-no-properties 2 line)))))
-        (parsenv-utils-read-file-into-list-of-lines file)))
-
-(defun parsenv-utils-load-env-var-from-file (var file &optional sep)
-  "Load into the environment variable VAR each line from FILE.
-Existing values will be maintained.  SEP is an optional separator."
-  (interactive)
-  (unless sep (setq sep path-separator))
-  (setenv var (concat (mapconcat 'convert-standard-filename
-                                 (parsenv-utils-read-file-into-list-of-lines file)
-                                 sep) sep (getenv var))))
 
 (provide 'parsenv-utils)
 ;;; parsenv-utils.el ends here
--- parsenv.el
+++ parsenv.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <enniomore@icloud.com>
 ;; Created: Monday, December  4, 2017
 ;; Version: 1.0
-;; Modified Time-stamp: <2017-12-05 08:32:59 dharms>
+;; Modified Time-stamp: <2017-12-05 17:48:53 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords: tools
 ;; URL: https://github.com/articuluxe/parsenv.git
@@ -28,6 +28,26 @@
 
 ;;; Code:
 (require 'parsenv-utils)
+
+(defun parsenv-utils-load-env-vars-from-file (file)
+  "Load each line from FILE, of the form `var=val'.
+For each line, sets environment variable `var' equal to `val'."
+  (interactive "fLoad environment variables from file: ")
+  (mapc (lambda(line)
+          (when (string-match "\\(.+\\)=\\(.*\\)" line)
+            (setenv (match-string-no-properties 1 line)
+                    (substitute-env-vars
+                     (match-string-no-properties 2 line)))))
+        (parsenv-utils-read-file-into-list-of-lines file)))
+
+(defun parsenv-utils-load-env-var-from-file (var file &optional sep)
+  "Load into the environment variable VAR each line from FILE.
+Existing values will be maintained.  SEP is an optional separator."
+  (interactive)
+  (unless sep (setq sep path-separator))
+  (setenv var (concat (mapconcat 'convert-standard-filename
+                                 (parsenv-utils-read-file-into-list-of-lines file)
+                                 sep) sep (getenv var))))
 
 (provide 'parsenv)
 ;;; parsenv.el ends here
